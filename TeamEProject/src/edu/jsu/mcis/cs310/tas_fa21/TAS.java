9b52b10683d69e5b75624ce262c71ba1345b9b34
Liam O'Kelley <lokelley@stu.jsu.edu>
2021-10-05 13:34:43 -0500

    Initial Commit/Added v1 of getShift() methods

--- TeamEProject/src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
+++ TeamEProject/src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
@@ -1,9 +1,7 @@
-
 package edu.jsu.mcis.cs310.tas_fa21;
 
 
 public class TAS {
-    
     public static void main(String[] args) {
         
     }
---
+++ TeamEProject/src/edu/jsu/mcis/cs310/tas_fa21/TASDatabase.java
@@ -0,0 +1,168 @@
+package edu.jsu.mcis.cs310.tas_fa21;
+
+import java.sql.*;
+import java.sql.Connection;
+
+
+
+public class TASDatabase {
+    
+    Connection conn = null;
+    PreparedStatement pstSelect = null, pstUpdate = null;
+    ResultSet resultset = null;
+    ResultSetMetaData metadata = null;
+    
+    boolean hasresults;
+    int columnCount = 0;
+    
+    public TASDatabase(){
+        
+        try{
+
+            /* Identify the Server */
+        
+            String server = ("jdbc:mysql://localhost/tas_fa21_v1");
+            String username = "tasuser";
+            String password = "teame";
+            System.out.println("Connecting to " + server + "...");
+
+            /* Load the MySQL JDBC Driver */
+        
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        
+            /* Open Connection */
+
+            conn = DriverManager.getConnection(server, username, password);
+        
+            /* Test Connection */
+
+            if(conn.isValid(0)){
+            System.out.println("Connection Successful");
+            }
+    
+            
+        }
+        
+        catch (Exception e) {
+            System.err.println(e.toString());
+        }
+        
+        // Close Database Objects
+        
+        finally {
+            
+            if (resultset != null) { try { resultset.close(); resultset = null;} catch (Exception e ) {} }
+            
+            if (pstSelect != null) { try { pstSelect.close(); pstSelect = null;} catch (Exception e ) {} }
+            
+            if (pstUpdate != null) { try { pstUpdate.close(); pstUpdate = null;} catch (Exception e ) {} }
+            
+        }
+    }
+    
+    public Badge getBadge(String badgeid){
+        try{
+            
+            pstSelect = conn.prepareStatement("SELECT * FROM badge where id = ?");
+            
+            pstSelect.setString(1, badgeid);
+            
+            pstSelect.execute();
+            resultset = pstSelect.getResultSet();
+            resultset.first();
+            
+            
+            //Results
+            String idNum = resultset.getString(1);
+            String name = resultset.getString(2);
+            
+            Badge b = new Badge(idNum, name);
+            
+            return b;
+        }
+        
+        catch(Exception e){
+            System.err.println("** getBadge: " + e.toString());
+        }
+        
+        return null;
+    }
+    
+    public Punch getPunch(int punchid) {
+        Timestamp originalTimeStamp = null;
+        Punch p = null;
+        
+        try {
+            pstSelect = conn.prepareStatement("select * from punch where id=?");
+            
+            pstSelect.execute();
+            resultset = pstSelect.getResultSet();
+            
+            resultset.first();
+            int punchID = resultset.getInt(1);
+            int punchTerminalID = resultset.getInt(2);
+            String punchBadge = resultset.getString(3);
+            originalTimeStamp = resultset.getTimestamp(4);
+            int punchTypeID = resultset.getInt(5);
+            
+            p = new Punch(punchID, punchTerminalID, punchBadge, originalTimeStamp, punchTypeID);
+        }
+        
+        catch(Exception e) {
+            System.err.println("** getPunch: " + e.toString());
+        }
+        
+        return p;
+    }
+    
+    
+    public Shift getShift(String shiftID) {
+        try {
+            //I think this method is correct but I can't test it yet without the Shift class
+            pstSelect = conn.prepareStatement("select * from employee where id=7");
+            
+            pstSelect.setString(7, shiftID);
+            
+            pstSelect.execute();
+            resultset = pstSelect.getResultSet();
+            resultset.first();
+            
+            //Results
+            String idNum = resultset.getString(7);
+            
+            Shift s = new Shift(idNum);
+            
+            return s;
+        }
+        
+        catch(Exception e) {
+            System.err.println("** getShift: " + e.toString());
+        }
+        return null;
+    }
+    
+    public Shift getShift(Badge b) {
+        try {
+            //Probably not correct yet, just something to work off of
+            pstSelect = conn.prepareStatement("select * from employee where id=1");
+            
+            pstSelect.setString(1, b);
+            
+            pstSelect.execute();
+            resultset = pstSelect.getResultSet();
+            resultset.first();
+            
+            //Results
+            String idNum = resultset.getString(1);
+            
+            Badge b = new Badge(idNum);
+            
+            return b;
+        }
+        
+        catch(Exception e) {
+            System.err.println("** getShift: " + e.toString());
+        }
+        return null;
+    }
+}
