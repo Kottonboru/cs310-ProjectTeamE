9c77b1ede4ec0291deeaabad02d4aae7dbc237db
keeskothemagician <kbrand@stu.jsu.edu>
2021-10-08 11:30:45 -0500

    added punch class

---
+++ TeamEProject/src/edu/jsu/mcis/cs310/tas_fa21/PunchType.java
@@ -0,0 +1,15 @@
+package edu.jsu.mcis.cs310.tas_fa21;
+
+/**
+ *10/06/21
+ * @author keeskothemagician
+ */
+public enum PunchType {
+    CLOCK_OUT("CLOCK OUT"),
+    CLOCK_IN("CLOCK IN"),
+    TIME_OUT("TIME OUT");
+    private final String description;
+    private PunchType(String d) { description = d; }
+    @Override
+    public String toString() { return description; }
+}
--- TeamEProject/src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
+++ TeamEProject/src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
@@ -1,7 +1,6 @@
 
 package edu.jsu.mcis.cs310.tas_fa21;
 
-
 public class TAS {
     
     public static void main(String[] args) {
