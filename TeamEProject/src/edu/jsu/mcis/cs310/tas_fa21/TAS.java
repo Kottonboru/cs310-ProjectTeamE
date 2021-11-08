625c6feb6f33bc7bdd5e1863433a30ccc6a8be09
Aaron Cotton <aaronroycotton@aol.com>
2021-11-08 12:22:32 -0600

    Feature 4 Test

--- TeamEProject/src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
+++ TeamEProject/src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
@@ -1,11 +1,30 @@
 package edu.jsu.mcis.cs310.tas_fa21;
 
-import java.math.BigDecimal;
-
+import java.time.LocalDateTime;
+import java.util.*;
+import java.time.temporal.ChronoUnit;
 
 public class TAS {
     public static void main(String[] args) {
-        BigDecimal d = new BigDecimal("0.1").add(new BigDecimal("0.2"));
-        System.out.println(d);
+    }
+    
+    public static int calculateTotalMinutes (ArrayList<Punch>dailypunchlist, Shift shift){
+        
+        int total = 0;
+        
+        for (Punch punch : dailypunchlist) {
+            
+            LocalDateTime begin;
+            LocalDateTime end;
+            
+            if (punch.getPunchtype() == PunchType.CLOCK_IN){ 
+                begin = punch.getAdjustedTimeStamp();
+                
+                
+                total = (int)(ChronoUnit.MINUTES.between(begin,end));
+            }
+            
+        } 
+        return total;
     }
 }
